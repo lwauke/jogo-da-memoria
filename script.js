b3f4f4daa95dfc854c162c09e287bf348d437154
lwauke <lwauke@hotmail.com>
2019-03-12 22:44:45 -0300

    logica

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const cards = document.querySelectorAll('.card')
 
 cards.forEach(function(element) {
     element.addEventListener('click', function(event) {
-        const alvo = event.currentTarget
-        alvo.dataset.status = "on"
-        console.log(alvo)
+        const target = event.currentTarget;
+        const activeCards = [].filter.call(cards, ({dataset: d}) => parseInt(d.status) && parseInt(d.game));
+
+        target.dataset.status = "1";
+
+        if(activeCards.length === 1) {
+            if(target.dataset.id === activeCards[0].dataset.id) {
+                activeCards[0].dataset.game = "out";
+                target.dataset.game = "out";
+            } else {
+                setTimeout(function() {
+                    activeCards[0].dataset.status = 0;
+                    target.dataset.status = 0;
+                }, 1000)
+            }
+        }
+        
+        console.log(activeCards)
     })
 })
