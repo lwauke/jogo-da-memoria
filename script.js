12f84e8213ed77a1dca8b588fdbd8be3637290bf
lwauke <lwauke@hotmail.com>
2019-03-12 22:32:20 -0300

    current target e alterando status

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const cards = document.querySelectorAll('.card')
 
-cards.forEach(console.log)
+cards.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+        const alvo = event.currentTarget
+        alvo.dataset.status = "on"
+        console.log(alvo)
+    })
+})
