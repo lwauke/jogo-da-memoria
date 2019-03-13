c16cbe23c28b22918cc24f30549af1dec04f2235
lwauke <lwauke@hotmail.com>
2019-03-12 23:31:21 -0300

    corrigindo bug de clique na mesma imagem e gerando cartões aleatoriamente

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
+const container = document.querySelector('.container');
+
+const arr = [
+    {id: 1, imagePath: "images/feliz.jpeg"},
+    {id: 2, imagePath: "images/nervoso.jpeg"},
+    {id: 3, imagePath: "images/surpreso.jpeg"},
+    {id: 4, imagePath: "images/risonho.jpeg"},
+];
+
+function randomIndex() {
+    return Math.ceil(Math.random() * 4) - 1;
+}
+
+function generateCard(obj) {
+    const {id, imagePath} = obj;
+
+    return `<div class="card" data-game="1" data-status="0" data-id="${id}">
+        <div class="back"></div>
+        <img src="${imagePath}" alt="">
+    </div>`;
+};
+
+function randomGenerate() {
+    if (!randomGenerate.cache) randomGenerate.cache = [];
+
+    const index = randomIndex();
+
+    const full = (randomGenerate.cache.filter(n => n === index) || {}).length === 2;
+
+
+    if (full) {
+        return randomGenerate() 
+    } else {
+        randomGenerate.cache.push(index);
+        return generateCard(arr[index]);
+    } 
+}
+
+let counter = 0;
+
+while(counter < arr.length * 2) {
+    container.innerHTML += randomGenerate();
+    counter++;
+}
+
 const cards = document.querySelectorAll('.card')
 
 cards.forEach(function(element) {
     element.addEventListener('click', function(event) {
         const target = event.currentTarget;
-        const activeCards = [].filter.call(cards, ({dataset: d}) => parseInt(d.status) && parseInt(d.game));
+
+        const activeCard = [].find.call(
+            cards, 
+            ({dataset: d}) => parseInt(d.status) && parseInt(d.game)
+        );
+
+        const equal = activeCard === target;
 
         target.dataset.status = "1";
 
-        if(activeCards.length === 1) {
-            if(target.dataset.id === activeCards[0].dataset.id) {
-                activeCards[0].dataset.game = "out";
+        if(activeCard) {
+            if(target.dataset.id === activeCard.dataset.id && !equal) {
+                activeCard.dataset.game = "out";
                 target.dataset.game = "out";
             } else {
                 setTimeout(function() {
-                    activeCards[0].dataset.status = 0;
+                    activeCard.dataset.status = 0;
                     target.dataset.status = 0;
                 }, 1000)
             }
         }
-        
-        console.log(activeCards)
     })
 })
